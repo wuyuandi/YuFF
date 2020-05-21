b9f82c95733f9dcf7c0a2a5b5c2fd620f7a39370
wuyuandi <yuandi@ualberta.ca>
2020-05-21 16:45:33 -0600

    nothing add

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,25 @@
+/*
+ * @Author: yuandi wu
+ * @Date: 2020-04-29 13:42:45
+ * @LastEditTime: 2020-05-21 13:12:18
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /YuFF/src/pages/index.js
+ */ 
+/*
+ *                   江城子 . 程序员之歌
+ * 
+ *               十年生死两茫茫，写程序，到天亮。
+ *                   千行代码，Bug何处藏。
+ *               纵使上线又怎样，朝令改，夕断肠。
+ * 
+ *               领导每天新想法，天天改，日日忙。
+ *                   相顾无言，惟有泪千行。
+ *               每晚灯火阑珊处，夜难寐，加班狂。
+ * 
+ */
+
 import React from "react"
-
-
 import Layout from "../components/layout"
 //import Image from "../components/image"
 import SEO from "../components/seo"
